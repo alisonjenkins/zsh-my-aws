552e88369e5d7b2406700f102168b7accb56b551
Alan Jenkins <alan.jenkins@uk.clara.net>
2020-02-14 08:51:39 +0000

    Fix import of instance commands

--- zsh-my-aws.plugin.zsh
+++ zsh-my-aws.plugin.zsh
@@ -1,8 +1,9 @@
-local PLUG_DIR="$(dirname \"${(%):-%N}\")"
+local PLUG_DIR
+PLUG_DIR=$(dirname ${(%):-%N})
 
 amr() {
     AWS_DEFAULT_REGION="$1"
     export AWS_DEFAULT_REGION
 }
 
-source "$PLUG_DIR/instance-commands"
+source "$PLUG_DIR/lib/instance-commands"
