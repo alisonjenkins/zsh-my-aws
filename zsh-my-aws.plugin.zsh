04f4f5ba5e3d5f33ef053d96542d67b15c393040
Alan Jenkins <alan.jenkins@uk.clara.net>
2020-02-14 10:00:46 +0000

    Enhance amr to allow viewing current region.

--- zsh-my-aws.plugin.zsh
+++ zsh-my-aws.plugin.zsh
@@ -1,8 +1,15 @@
 local PLUG_DIR=$(dirname ${(%):-%N})
 
 amr() {
-    AWS_DEFAULT_REGION="$1"
-    export AWS_DEFAULT_REGION
+    if [ -z "$1" ]; then
+        if [ -z "$AWS_DEFAULT_REGION" ]; then
+            echo "AWS_DEFAULT_REGION is not set."
+        fi
+        echo "$AWS_DEFAULT_REGION"
+    else
+        AWS_DEFAULT_REGION="$1"
+        export AWS_DEFAULT_REGION
+    fi
 }
 
 source "$PLUG_DIR/lib/instance-commands"
